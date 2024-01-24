274be4713ed4ace522c8aec675fd334c77f131b9
JulianYepe <julian.yepe@skyline.be>
2024-01-24 16:05:25 -0500

    init commit

--- GetAllElementsInService_1/GetAllElementsInService_1.cs
+++ GetAllElementsInService_1/GetAllElementsInService_1.cs
@@ -51,26 +51,146 @@ DATE		VERSION		AUTHOR			COMMENTS
 
 namespace GetAllElementsInService_1
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Globalization;
-	using System.Text;
-	using Skyline.DataMiner.Automation;
+    using System;
+    using System.Collections.Generic;
     using Skyline.DataMiner.Analytics.GenericInterface;
+    using Skyline.DataMiner.Net.Messages;
 
+    /// <summary>
+    /// Represents a data source for retrieving all elements in a service.
+    /// Implements the IGQIDataSource, IGQIOnInit, and IGQIInputArguments interfaces.
+    /// </summary>
+    /// <remarks>
+    /// This class is used to interact with the DataMiner System (DMS) to retrieve service and element information.
+    /// It provides methods for retrieving columns, getting the next page of data, initializing the data source,
+    /// retrieving input arguments, and processing provided arguments.
+    /// </remarks>
     [GQIMetaData(Name = "Get All Elements in service")]
-    public class MyDataSource : IGQIDataSource
+    public class MyDataSource : IGQIDataSource, IGQIOnInit, IGQIInputArguments
     {
+        /// <summary>
+        /// Represents the service name argument required for the data source.
+        /// </summary>
+        /// <remarks>
+        /// This argument is used to specify the name of the service from which the elements are to be retrieved.
+        /// It is a required argument for the data source.
+        /// </remarks>
+        private readonly GQIStringArgument _serviceNameArgument = new GQIStringArgument("Service") { IsRequired = true };
+
+        /// <summary>
+        /// Represents the DataMiner System (DMS) used for sending messages and retrieving service and element information.
+        /// </summary>
         private GQIDMS _dms;
-        
+
+        /// <summary>
+        /// Represents the name of the service for which the elements are being retrieved.
+        /// </summary>
+        /// <remarks>
+        /// This field is populated with the value provided by the user through the GQIStringArgument '_serviceNameArgument'.
+        /// It is used in the 'GetNextPage' method to send a 'GetServiceByNameMessage' to the DataMiner System (DMS).
+        /// </remarks>
+        private string _serviceName;
+
+        /// <summary>
+        /// Retrieves the columns for the data source.
+        /// </summary>
+        /// <remarks>These are the columns the user will see on the low code app.</remarks>
+        /// <returns>
+        /// An array of GQIColumn objects representing the columns of the data source.
+        /// </returns>
         public GQIColumn[] GetColumns()
         {
-            throw new NotImplementedException();
+            return new GQIColumn[]
+            {
+                new GQIStringColumn("Element Name"),
+            };
         }
 
+        /// <summary>
+        /// Retrieves the next page of data from the data source.
+        /// </summary>
+        /// <param name="args">The arguments required to get the next page.</param>
+        /// <returns>
+        /// A GQIPage object containing the next page of data. The 'HasNextPage' property of the returned object indicates whether there are more pages to retrieve.
+        /// </returns>
+        /// <remarks>
+        /// This method sends a 'GetServiceByNameMessage' to the DataMiner System (DMS) to get the service information.
+        /// It then iterates over the elements in the service, sending a 'GetElementByIDMessage' for each element to get its information.
+        /// The element information is added as a row to the GQIPage object that is returned.
+        /// </remarks>
         public GQIPage GetNextPage(GetNextPageInputArgs args)
         {
-            throw new NotImplementedException();
+            var rows = new List<GQIRow>();
+            var message = new GetServiceByNameMessage(_serviceName);
+
+            try
+            {
+                if (!(_dms.SendMessage(message) is ServiceInfoEventMessage service))
+                    return new GQIPage(rows.ToArray()) { HasNextPage = false };
+
+                var elementInfo = service.ServiceParams;
+
+                foreach (var info in elementInfo)
+                {
+                    var dmaId = info.DataMinerID;
+                    var elementId = info.ElementID;
+                    var elementMessage = new GetElementByIDMessage(dmaId, elementId);
+
+                    if (_dms.SendMessage(elementMessage) is ElementInfoEventMessage element)
+                    {
+                        var row = new GQIRow(
+                            new[]
+                            {
+                                new GQICell {Value = element.Name},
+                            });
+
+                        rows.Add(row);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Something went wrong.
+            }
+
+            return new GQIPage(rows.ToArray()) { HasNextPage = false };
+        }
+
+        /// <summary>
+        /// Initializes the data source with the provided arguments.
+        /// </summary>
+        /// <remarks>The first method that executes in the script.</remarks>
+        /// <param name="args">The initialization arguments.</param>
+        /// <returns>Returns an instance of OnInitOutputArgs after initialization.</returns>
+        public OnInitOutputArgs OnInit(OnInitInputArgs args)
+        {
+            _dms = args.DMS;
+            return new OnInitOutputArgs();
+        }
+
+        /// <summary>
+        /// Retrieves the input arguments required for the data source.
+        /// </summary>
+        /// <remarks>
+        /// These are the arguments that the user will need to provide for the data source.
+        /// </remarks>
+        /// <returns>
+        /// An array of GQIArgument objects representing the input arguments for the data source.
+        /// </returns>
+        public GQIArgument[] GetInputArguments()
+        {
+            return new GQIArgument[] { _serviceNameArgument };
+        }
+
+        /// <summary>
+        /// Processes the provided arguments after they have been inputted.
+        /// </summary>
+        /// <param name="args">The arguments that have been processed.</param>
+        /// <returns>Returns an instance of OnArgumentsProcessedOutputArgs after processing the arguments.</returns>
+        public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
+        {
+            _serviceName = args.GetArgumentValue(_serviceNameArgument);
+            return new OnArgumentsProcessedOutputArgs();
         }
     }
 }
